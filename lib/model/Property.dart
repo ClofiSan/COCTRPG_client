bab035b77a41e5503413f8e968a3940b3bd60ff6
Unknown <z731630537@163.com>
2019-11-13 18:03:59 +0800

    主页基本功能实现，还需要排版

--- lib/model/Property.dart
+++ lib/model/Property.dart
@@ -1,33 +1,4 @@
-class Property{
-  String _label;
-  String _name;
-  int _value;
-
-  Property(this._label, this._name, this._value);
-
-
-  @override
-  String toString() {
-    return 'Property{_label: $_label, _name: $_name, _value: $_value}';
-  }
-
-  int get value => _value;
-
-  set value(int value) {
-    _value = value;
-  }
-
-  String get name => _name;
-
-  set name(String value) {
-    _name = value;
-  }
-
-  String get label => _label;
-
-  set label(String value) {
-    _label = value;
-  }
-
-
+import 'Attribute.dart';
+class Property extends Attribute{
+  Property();
 }
---
+++ lib/page/note/note_list_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class NoteListPage extends StatefulWidget{
+  NoteListPage({Key key}): super(key: key);
+
+  @override
+  _NoteListPage createState() => _NoteListPage();
+
+}
+
+
+class _NoteListPage extends State<NoteListPage>{
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
+
+
