b2c4da27dbdff77344c30a0549d93aec580fa627
Ventura <z731630537@163.com>
2020-04-17 03:57:09 +0800

    准备引入provider

--- lib/create/page/skill_chosen_page.dart
+++ lib/create/page/skill_chosen_page.dart
@@ -13,12 +13,15 @@ class SkillChosenPage extends StatefulWidget{
 
 class _SkillChosenPage extends State<SkillChosenPage>{
 
+  GlobalKey<FormState> _formKey= new GlobalKey<FormState>();
 
   List<String> skillTypeList = List();
   Map<String,dynamic> skillMap = Map();
   PageController pageController;
   PageView pageView;
   int currentPage;
+
+  TextEditingController _controller = TextEditingController();
 
   @override
   void initState() {
@@ -67,63 +70,117 @@ class _SkillChosenPage extends State<SkillChosenPage>{
                       showModalBottomSheet(
                         context: context,
                         builder: (BuildContext context) {
-                          return Container(
-                            height: MediaQuery.of(context).size.width,
-                            child: Flex(
-                              direction: Axis.horizontal,
-                              children: <Widget>[
-                                Expanded(
-                                  flex: 1,
-                                  child: Column(
-                                    children: <Widget>[
-                                      Text(sameSkills[index].label,style: TextStyle(fontSize: 20,color: Colors.white),),
-                                    ],
-                                  ),
+                          return StatefulBuilder(builder: (context,StateSetter setState){
+                            return Container(
+                                padding: EdgeInsets.all(20),
+                                decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                      colors: [Color(0xFF20BAC1), Colors.grey[600]],
+                                      begin: Alignment.topCenter,
+                                      end: Alignment.bottomCenter,
+                                    )
                                 ),
-                                Expanded(
-                                  flex: 3,
-                                  child: Column(
-                                    children: <Widget>[
-                                      Row(
-                                        children: <Widget>[
-                                          Text("初始"),
-                                          Text(sameSkills[index].initial.toString())
-                                        ],
-                                      ),
-                                      Row(
-                                        children: <Widget>[
-                                          Text("职业"),
-                                          Text(sameSkills[index].professionalPoint.toString())
-                                        ],
-                                      ),
-                                      Row(
-                                        children: <Widget>[
-                                          Text("兴趣"),
-                                          Text(sameSkills[index].interestPoint.toString())
-                                        ],
-                                      ),
-                                      Row(
-                                        children: <Widget>[
-                                          Text("总值"),
-                                          Text(sameSkills[index].value.toString())
-                                        ],
-                                      ),
-                                      Wrap(
-                                        children: <Widget>[
-                                          Text("描述"),
-                                          sameSkills[index].description!=null?Text(sameSkills[index].description):Text("无"),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ),
+                                height: MediaQuery.of(context).size.height*0.5,
+                                child: Form(
+                                    key: _formKey,
+                                    child:Column(
+                                      children: <Widget>[
+                                        Text(sameSkills[index].label,style: TextStyle(fontSize: 20,color: Colors.white),),
+                                        Row(
+                                          children: <Widget>[
+                                            Text("初始"),
+                                            Container(
+                                              width: MediaQuery.of(context).size.width*0.4,
+                                              height: 30,
+                                              child: TextField(
+                                                controller: _controller,
+                                                textAlign: TextAlign.center,
+                                                decoration: InputDecoration(
+                                                  hintText: sameSkills[index].initial.toString(),
+                                                    hintStyle: TextStyle(fontSize: 16,color: Colors.grey),
+                                                    border: InputBorder.none,
+                                                    fillColor: AppTheme.investigatorMinorColor,
+                                                    filled: true,
+                                                    errorStyle: TextStyle(color: Colors.red[600])
+                                                ),
+                                                onChanged: (v){
+                                                  setState(() {
+                                                    sameSkills[index].initial = v as int;
+                                                    print(v);
+                                                  });
+                                                },
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: <Widget>[
+                                            Text("职业"),
+                                            Text(
+                                                sameSkills[index].professionalPoint.toString(),
+                                                style: TextStyle(color: Colors.white)
+                                            ),
 
-                              ],
-                            ),
-                            decoration: BoxDecoration(
-                              color: AppTheme.investigatorMainColor
-                            ),
-                          );
+                                          ],
+                                        ),
+                                        Row(
+                                          children: <Widget>[
+                                            Text("兴趣"),
+                                            Text(
+                                                sameSkills[index].interestPoint.toString(),
+                                                style: TextStyle(color: Colors.white)
+                                            )
+                                          ],
+                                        ),
+                                        Row(
+                                          children: <Widget>[
+                                            Text("成长"),
+                                            Text(
+                                                sameSkills[index].value.toString(),
+                                                style: TextStyle(color: Colors.white)
+                                            )
+                                          ],
+                                        ),
+                                        Row(
+                                          children: <Widget>[
+                                            Text("判定"),
+                                            Text(
+                                                "${sameSkills[index].value/1}/${sameSkills[index].value/2}/${sameSkills[index].value/5}",
+                                                style: TextStyle(color: Colors.white)
+                                            )
+                                          ],
+                                        ),
+                                        Wrap(
+                                          children: <Widget>[
+                                            Text("描述"),
+                                            Container(
+                                              alignment: Alignment.centerLeft,
+                                              padding: EdgeInsets.all(5),
+                                              color: AppTheme.investigatorMinorColor,
+                                              child: sameSkills[index].description!=null?
+                                              Text(
+                                                sameSkills[index].description,
+                                                style: TextStyle(color: Colors.white),)
+                                                  :Text("无"),
+                                            ),
+                                          ],
+                                        ),
+                                        FlatButton(
+                                          child: Container(
+                                            child: Text("确定"),
+                                          ),
+                                          onPressed: (){
+                                            setState(() {
+                                              sameSkills[index].initial = 30;
+                                            });
+
+                                          },
+                                        ),
+                                      ],
+                                    )
+                                )
+                            );
+                          });
                         },
                       );
                     },
